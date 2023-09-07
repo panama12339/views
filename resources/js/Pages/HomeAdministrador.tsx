d68b44c5881a9c69b2553195d44279fe903e5d9a
cdmm299 <carlosmendizabal299@gmail.com>
2023-09-07 15:26:19 -0400

    colores del sistema fueron añadidos al diseño de tailwind

--- resources/js/Pages/HomeAdministrador.tsx
+++ resources/js/Pages/HomeAdministrador.tsx
@@ -6,7 +6,7 @@ export default function HomeAdministrador() {
         <AppLayout
           title="Administrador"
           renderHeader={() => (
-            <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight bg-customVerdeOscuro">
               Pagina Inicial Administrador
             </h2>
           )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,33 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.tsx',
-    ],
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './vendor/laravel/jetstream/**/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.tsx',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    colors: {
+      customMoradoClaro: '#d1bcde',
+      customVerdeClaro: '#8ebbc5',
+      customMoradoOscuro: '#7d4c8a',
+      customMoradoMedio: '#cb6ce6',
+      customRosado: '#faacde',
+      customNaranja: '#f8c2a1',
+      customVerdeOscuro: '#19757c',
+      //para blanco usar white o
+      customBlanco: '#ffffff',
     },
 
-    plugins: [forms, typography],
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+
+  plugins: [forms, typography],
 };
