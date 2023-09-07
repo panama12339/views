9a007eea8eed7c9cd40468f94c3e0b4ddf161a9c
cdmm299 <carlosmendizabal299@gmail.com>
2023-09-06 21:34:07 -0400

    implementacion parcial de lenguajes y cambio de lenguaje predeterminado a español

---
+++ lang/es/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+  /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+  'reset' => 'Tu contraseña ha sido reseteada.',
+  'sent' =>
+    'Te hicimos el envío del link de reseteo de contraseña a tu correo.',
+  'throttled' => 'Por favor, espera antes de reintentar.',
+  'token' => 'Este token de reseteo de contraseña es invalido.',
+  'user' => 'No encontramos un usuario con ese correo',
+];
