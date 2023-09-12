9b66c12fa3683b9d3771eaa0dbb403af2e7390b3
cdmm299 <carlosmendizabal299@gmail.com>
2023-09-11 21:02:38 -0400

    estilos incompleto

--- resources/js/Layouts/LogoLayout.tsx
+++ resources/js/Layouts/LogoLayout.tsx
@@ -23,14 +23,23 @@ export default function AppLayout({
 
 
 
-        <nav className="m-2 bg-white dark:bg-gray-800">
+        <nav className="dark:bg-gray-800">
             
-                <div className="grid grid-cols-2 gap-20 max-h-24">
+                <div className="grid grid-cols-3">
                         {/* <!-- Logo Gabinete Sanacion de la Conducta con fondo --> */}
-                        <img className="justify-self-star self-center max-h-24" src='http://localhost:8000/images/logoGabineteSanacionConducta.png'></img>
+                        <img className=" p-4 justify-self-start self-center" src='http://localhost:8000/images/logoGabineteSanacionConducta.png'></img>
                        
-                        {/* <!-- Logo Fundacion Educar con fondo --> */}
-                        <img className="justify-self-end self-center max-h-24 bg-customMoradoClaro" src='http://localhost:8000/images/logoFundacionEducar.png'></img> 
+                        
+                        <div className='bg-customMoradoClaro' >
+                          <div className="h-0 w-0 border-t-[50px] border-l-[75px] border-b-[50px] 
+                        border-solid border-t-customMoradoClaro border-b-customMoradoClaro border-l-customBlanco"></div>
+                        </div>
+                        <div className='bg-customMoradoClaro grid grid-cols-4 p-4'>
+                              {/* <!-- Logo Fundacion Educar con fondo --> */}
+                              <img className="justify-self-start self-center col-span-3" src='http://localhost:8000/images/logoFundacionEducar.png'></img>
+                        <img className=" justify-self-center self-center max-h-12" src='http://localhost:8000/images/bolivia.png'></img> 
+                        </div>
+                        
                 </div>
 
         </nav>
